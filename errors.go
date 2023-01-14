0ae0fb1b14efb0870ff9e5149f7892929c66b95d
morrisxyang <morris.x.yang@gmail.com>
2023-01-14 11:50:18 +0800

    fundamentalError StackTrace

--- errors.go
+++ errors.go
@@ -8,11 +8,11 @@ import (
 
 // fundamentalError 定义了包含 stack 的 error
 type fundamentalError struct {
-	cause  error  // cause 内部嵌套错误, 构造错误链
-	code   int    // code 传入的错误码, 选填
-	msg    string // msg 传入的错误描述, 可对外暴露
-	detail string // detail 在错误描述 msg 的基础上, 增加文件名、行数、调用函数名等信息, 不对外暴露, 服务内部使用
-	// stack  errors.StackTrace // stack 错误堆栈, 如果内部嵌套错误 cause 已有堆栈, 则不再设置
+	cause  error      // cause 内部嵌套错误, 构造错误链
+	code   int        // code 传入的错误码, 选填
+	msg    string     // msg 传入的错误描述, 可对外暴露
+	detail string     // detail 在错误描述 msg 的基础上, 增加文件名、行数、调用函数名等信息, 不对外暴露, 服务内部使用
+	stack  StackTrace // stack 错误堆栈, 如果内部嵌套错误 cause 已有堆栈, 则不再设置
 }
 
 // Error 实现 Error 接口, 打印链路 detail 信息, 包含文件名、行数等
@@ -47,6 +47,12 @@ func (fd *fundamentalError) Msg() string {
 
 // Format 实现 Format 接口
 func (fd *fundamentalError) Format(s fmt.State, verb rune) {
+	var stackTrace StackTrace
+	defer func() {
+		if stackTrace != nil {
+			stackTrace.Format(s, verb)
+		}
+	}()
 	switch verb {
 	case 'v':
 		if s.Flag('+') {
@@ -55,6 +61,9 @@ func (fd *fundamentalError) Format(s fmt.State, verb rune) {
 			}
 			if fd.Cause() != nil {
 				_, _ = fmt.Fprintf(s, "\nCause by %+v", fd.Cause())
+			}
+			if fd.stack != nil {
+				stackTrace = fd.stack
 			}
 			return
 		}
