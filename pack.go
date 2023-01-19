f6d7e727850c2636cdfe00fdca6d1534fe395005
morrisxyang <morris.x.yang@gmail.com>
2023-01-19 19:20:11 +0800

    add Newf() and NewWithCode()

--- pack.go
+++ pack.go
@@ -12,3 +12,22 @@ func New(msg string) error {
 		stack:  callers(),
 	}
 }
+
+// Newf 使用 format 格式的信息创建一个携带堆栈的错误
+func Newf(format string, args ...interface{}) error {
+	return &fundamentalError{
+		msg:    fmt.Sprintf(format, args...),
+		detail: fmt.Sprintf("%v, %v", callerFuncInfo(), fmt.Sprintf(format, args...)),
+		stack:  callers(),
+	}
+}
+
+// NewWithCode 使用传入的信息创建一个携带堆栈的错误
+func NewWithCode(code int, msg string) error {
+	return &fundamentalError{
+		msg:    msg,
+		detail: fmt.Sprintf("%v, %v", callerFuncInfo(), msg),
+		stack:  callers(),
+		code:   code,
+	}
+}
